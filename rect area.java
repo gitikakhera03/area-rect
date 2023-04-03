24170817d42a1e5a6696a69040185a5d7c6d765b
gitikakhera03 <92471993+gitikakhera03@users.noreply.github.com>
2023-04-03 12:26:55 +0530

    Add files via upload

---
+++ rect area.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.util.Scanner;
+
+public class rect area
+{
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        System.out.println("the area of the given rectangle is"
+                + (x * y));
+    }
+}
